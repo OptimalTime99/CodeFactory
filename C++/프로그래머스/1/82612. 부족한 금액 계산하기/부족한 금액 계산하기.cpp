c07403e3951cf4727958f252e7a128033215de69
OptimalTime99 <optimal9744@gmail.com>
2025-12-29 09:50:35 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = money;
+    
+    for (int i = 1; i <= count; ++i)
+    {
+        answer -= price * i;
+    }
+
+    return answer < 0 ? abs(answer) : 0;
+}
