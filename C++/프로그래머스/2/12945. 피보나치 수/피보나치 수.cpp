a298be6d1e877c54b6f9609cd0c01215f0d9bcbf
ChoiJunHyeok <optimal9744@gmail.com>
2026-04-27 09:55:50 +0900

    [level 2] Title: 피보나치 수, Time: 0.54 ms, Memory: 4.13 MB -BaekjoonHub

--- C++/프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ C++/프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -2,18 +2,14 @@
 
 using namespace std;
 
-vector<long long> memo(100001, -1);
-
-int fibo(int n)
-{
-    if (n <= 1) return n;
-    if (memo[n] != -1) return memo[n];
-    
-    memo[n] = (fibo(n - 1) + fibo(n - 2)) % 1234567;
-    
-    return memo[n];
-}
-
 int solution(int n) {
-    return fibo(n);
+    vector<int> f(n + 1);
+    f[0] = 0;
+    f[1] = 1;
+    
+    for (int i = 2; i <= n; i++) {
+        f[i] = (f[i - 1] + f[i - 2]) % 1234567;
+    }
+    
+    return f[n];
 }
